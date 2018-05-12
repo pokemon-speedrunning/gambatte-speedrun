a1ac0617b271721b96b0b820841904837a13977e
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2018-05-13 08:33:54 +1200

    Nuke SDL - it's not supported by GSR at all

--- clean.sh
+++ clean.sh
@@ -3,9 +3,6 @@
 echo "cd gambatte_qt && make distclean"
 (cd gambatte_qt && make distclean)
 
-echo "cd gambatte_sdl && scons -c"
-(cd gambatte_sdl && scons -c)
-
 echo "cd libgambatte && scons -c"
 (cd libgambatte && scons -c)
 
