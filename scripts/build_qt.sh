c7e397ff0b872ca7f67c13a6ad22dfb9621e7b89
Brandon Mulcahy <brandon@lightcones.net>
2018-10-21 16:58:27 -0400

    Add missing qmake command to build_qt scripts

--- scripts/build_qt.sh
+++ scripts/build_qt.sh
@@ -4,4 +4,4 @@ echo "cd libgambatte && scons"
 (cd libgambatte && scons) || exit
 
 echo "cd gambatte_qt && make"
-(cd gambatte_qt && rm -f bin/gambatte_qt.exe && make)
+(cd gambatte_qt && rm -f bin/gambatte_qt.exe && qmake && make)
