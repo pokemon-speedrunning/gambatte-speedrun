8ee89a48cfc09b1069d133fd9fb1f76239b9a12f
Stewart Marshall <4035468+Dabomstew@users.noreply.github.com>
2020-04-13 16:08:24 +1200

    Fix build scripts (#39)
    * Update build_qt.sh and build_qt_only.sh with the new filename for the GSR binary

--- scripts/build_qt.sh
+++ scripts/build_qt.sh
@@ -4,4 +4,4 @@ echo "cd libgambatte && scons"
 (cd libgambatte && scons) || exit
 
 echo "cd gambatte_qt && make"
-(cd gambatte_qt && rm -f bin/gambatte_qt.exe && qmake && make)
+(cd gambatte_qt && rm -f bin/gambatte_speedrun.exe && qmake && make)
