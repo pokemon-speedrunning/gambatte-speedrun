042d9555fea023b3051571d5c9ac2d5987055796
CasualPokePlayer <CasualPokePlayer@github.com>
2020-09-13 15:01:58 -0700

    Fix MBC3+RTC invalid "bank" switches

--- libgambatte/src/savestate.h
+++ libgambatte/src/savestate.h
@@ -82,6 +82,7 @@ struct SaveState {
 		unsigned char /*bool*/ IME;
 		unsigned char /*bool*/ halted;
 		unsigned char /*bool*/ enableRam;
+		unsigned char /*bool*/ mbcLockup;
 		unsigned char /*bool*/ rambankMode;
 		unsigned char /*bool*/ hdmaTransfer;
 		unsigned char /*bool*/ biosMode;
