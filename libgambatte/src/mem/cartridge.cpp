84a9e3a0d68b716536f485bc565f29ef150cf7c9
CasualPokePlayer <CasualPokePlayer@github.com>
2020-09-19 21:07:07 -0700

    Revert "More clean-up with MBC30 implementation (yeet that bool)"
    This reverts commit e6b81d2f402283954ece883375e810af48bd5820.

--- libgambatte/src/mem/cartridge.cpp
+++ libgambatte/src/mem/cartridge.cpp
@@ -294,13 +294,14 @@ private:
 
 class Mbc3 : public DefaultMbc {
 public:
-	Mbc3(MemPtrs &memptrs, Rtc *const rtc)
+	Mbc3(MemPtrs &memptrs, Rtc *const rtc, bool mbc30)
 	: memptrs_(memptrs)
 	, rtc_(rtc)
 	, rombank_(1)
 	, rambank_(0)
 	, enableRam_(false)
 	, mbcLockup_(false)
+	, mbc30_(mbc30)
 	{
 	}
 
@@ -324,8 +325,12 @@ public:
 			break;
 		case 2:
 			rambank_ = data;
-			if(!rtc_)
+			if(!mbc30_ && !rtc_) {
+				rambank_ = rambank_ & 0x03;
+			}
+			if(mbc30_ && !rtc_) {
 				rambank_ = rambank_ & 0x07;
+			}
 			if(rtc_) {
 				rambank_ = rambank_ & 0x0F;
 				mbcLockup_ = rambank_ > (rambanks(memptrs_) - 1) && rambank_ < 0x08 || rambank_ > 0x0C;
@@ -362,6 +367,7 @@ private:
 	unsigned char rombank_;
 	unsigned char rambank_;
 	bool enableRam_;
+	bool mbc30_;
 	bool mbcLockup_;
 
 	void setRambank() const {
@@ -818,6 +824,8 @@ LoadRes Cartridge::loadROM(std::string const &romfile,
 	rtc_.set(false, 0);
 	huc3_.set(false);
 
+	bool mbc30 = rambanks > 0x04;
+
 	rom->rewind();
 	rom->read(reinterpret_cast<char*>(memptrs_.romdata()), filesize / rombank_size() * rombank_size());
 	std::memset(memptrs_.romdata() + filesize / rombank_size() * rombank_size(),
@@ -841,7 +849,7 @@ LoadRes Cartridge::loadROM(std::string const &romfile,
 		break;
 	case type_mbc2: mbc_.reset(new Mbc2(memptrs_)); break;
 	case type_mbc3:
-		mbc_.reset(new Mbc3(memptrs_, hasRtc(memptrs_.romdata()[0x147]) ? &rtc_ : 0));
+		mbc_.reset(new Mbc3(memptrs_, hasRtc(memptrs_.romdata()[0x147]) ? &rtc_ : 0, mbc30));
 		break;
 	case type_mbc5: mbc_.reset(new Mbc5(memptrs_)); break;
 	case type_huc1: mbc_.reset(new HuC1(memptrs_)); break;
